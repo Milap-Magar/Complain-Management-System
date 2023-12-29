48712b9dfb08eccbfe31ba1321fef45c58c37ca8
milap-magar <milapmagar12@gmail.com>
2023-12-29 16:52:19 +0545

    Added: Form options segments

--- Frontend/src/pages/Form.js
+++ Frontend/src/pages/Form.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+  const [ value, setValue ] = useState("");
+  const options = [
+    {faculty: "BCA", id: 1 },
+    {faculty: "BBA", id: 2 },
+    {faculty: "BIM", id: 3 },
+    {faculty: "BIT", id: 4 },
+    {faculty: "BSc.CSIT", id: 5 },
+    {faculty: "BSW", id: 6 }
+  ];
+
+  const handleSubmit = (e) => {
+    setValue(e.target.value);
+  }
+
   return (
     <>  
       <h1>Complain Form</h1>
-      <form>
-        <span>faculty</span>
-        <select>
-          <option>BCA</option>
-          <option>BBA</option>
-          <option>BIT</option>
-          <option>BSc.CSIT</option>
-          <option>BIM</option>
+      <form >
+        <span>Faculty:</span>
+        <select name='faculty'>
+          {options.map(faculty => (
+            <option key={faculty.id} value={faculty.value} onSubmit={handleSubmit}>{faculty.faculty}</option>  
+          ))
+        }
         </select>
+        <p>{value}</p>
       </form>
     </>
   )
