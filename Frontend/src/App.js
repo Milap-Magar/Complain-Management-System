482a5bbe81af726af76ac7b55d0b04fe5e4ef2e7
milap-magar <milapmagar12@gmail.com>
2023-12-29 07:30:23 +0545

    Created: Form Page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './pages/Login.page';
+import Form from './pages/Form';
 import "./index.css"
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={ <Login /> } />
+          <Route path='/' element={ <Form /> } />
         </Routes>
       </BrowserRouter>
     </>
---
+++ Frontend/src/pages/Form.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <>  
+      <h1>Complain Form</h1>
+      <form>
+        <span>faculty</span>
+        <select>
+          <option>BCA</option>
+          <option>BBA</option>
+          <option>BIT</option>
+          <option>BSc.CSIT</option>
+          <option>BIM</option>
+        </select>
+      </form>
+    </>
+  )
+}
+
+export default Form
