863ad6991de44a9ccc1aaf04d959dcc48fcab5b2
milap-magar <milapmagar12@gmail.com>
2024-01-11 13:24:23 +0545

    Added: Images and Various Sections

--- Frontend/src/pages/Login.page.jsx
+++ Frontend/src/pages/Login.page.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import image from "../assets/login.png";
+import bg_image from "../assets/background1.jpg";
+import { IoMdLogIn } from "react-icons/io";
 import "./Page.css";
 
 const Login = () => {
@@ -13,48 +15,69 @@ const Login = () => {
 
   return (
     <>
-    <div className={`max-w-full max-h-screen flex justify-center items-center`}>
-      <form action="" className={`bg-afno-200 w-full h-screen`}>
-        <div className={`flex flex-col justify-center items-center`}>
-          <figure className={``}>
-            <img src={image} alt="" />
-          </figure>
-          <div className={`flex flex-col justify-center w-90vw`}>
-            {/* symbol section */}
-            <label htmlFor="symbol">Enter your Symbol Number:</label>
-            <input
-              type="number"
-              name=""
-              className={` w-[250px] border-[2px]`}
-            />
-            {/* password section */}
-            <label htmlFor="password">Enter your Password: </label>
-            <input
-              type={showPwd}
-              name=""
-              className={`w-[250px] border-[2px]`}
-            />
-            <div className={``}>
+      <div
+        className={`min-w-full min-h-[100vh] flex justify-center items-center relative`}
+      >
+        <img
+          src={bg_image}
+          alt=""
+          className="w-full h-screen  absolute object-fill z-0"
+        />
+        <form
+          className={`bg-transparent border-[1px] border-slate-200 z-1 rounded-2xl w-[80vw] sm:w-[50vw] md:w-[40vw] h-[450px] md:h-[495px] shadow-2xl shadow-slate-950 relative`}
+        >
+          <div className={`flex flex-col justify-center items-center`}>
+            <figure className={`w-auto h-[200px]`}>
+              <img src={image} alt="main image" />
+            </figure>
+            <div
+              className={`flex flex-col justify-center w-90vw sm:gap-[0.5px] md:gap-1 py-1 md:py-8 cursor-pointer`}
+            >
+              <h1 className="flex justify-center items-center text-lg font-jet-mono font-extrabold">
+                Complain Management System
+              </h1>
+
+              {/* symbol section */}
+              <label htmlFor="symbol">Enter your Symbol Number:</label>
               <input
-                type="checkbox"
+                placeholder="Symbol Number"
+                type="number"
                 name=""
-                id=""
-                checked={isChecked}
-                onClick={handleShowPassword}
+                className={`text-sm p-[4px] w-[250px] rounded-sm border border-slate-950 bg-transparent font-epic text-white`}
               />
-              <span>Show Password</span>
-            </div>
-            <div className={``}>
-              <button
-                type="submit"
-                className={`border rounded-xl bg-afno-300 py-1 px-3 flex`}
-              >
-                Login
-              </button>
+              {/* password section */}
+              <label htmlFor="password">Enter your Password: </label>
+              <input
+                type={showPwd}
+                placeholder="Password"
+                name=""
+                className={`text-sm p-[4px] w-[250px] rounded-sm border border-slate-950 bg-transparent font-epic text-white`}
+              />
+              <div className={`cursor-pointer`}>
+                <input
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked={isChecked}
+                  onClick={handleShowPassword}
+                />
+                <span className="font-jet-mono text-sm">Show Password</span>
+              </div>
+              <div className={`flex justify-center items-center`}>
+                <button
+                  type="submit"
+                  className={`border rounded-xl bg-afno-300 m-[10px] md:m-3 py-1 px-3 flex border-slate-800 hover:bg-black hover:text-white font-epic font-extrabold text-md`}
+                >
+                  Login
+                  <IoMdLogIn className="w-6 h-6" />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+          <span className={`text-xs font-jet-mono text-slate-200 absolute bottom-0 left-2`}>
+            NOTE: This is only for PMC students.
+          </span>
+        </form>
       </div>
     </>
   );
