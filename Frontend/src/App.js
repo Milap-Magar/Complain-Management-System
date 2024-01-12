1ce1859730d0270c86a3f7b3b1b9e1bd009dbe29
milap-magar <milapmagar12@gmail.com>
2024-01-12 15:47:19 +0545

    Files Arranged

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './pages/Login.page';
-import Form from './pages/Form';
 import "./index.css"
 
 const App = () => {
